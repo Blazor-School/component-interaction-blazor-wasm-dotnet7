9af6c65626d0bf00a644d45a5f108d1935cf4607
Red Wei <huy.tran@blazorschool.com>
2023-01-21 11:40:19 +0700

    Add project files.

---
+++ ComponentInteraction/Program.cs
@@ -0,0 +1,13 @@
+using ComponentInteraction;
+using ComponentInteraction.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<ExampleTransferService>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
